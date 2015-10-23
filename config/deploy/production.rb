5d939ba90e5e96e72ff7b00338e4556223e30a78
takashyx <takashyx@gmail.com>
2015-10-23 18:16:07 +0900

    Fix: deploy from master branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
-set :branch, 'deploy_to_ec2_user'
+set :branch, 'master'
 
 
 role :app, "#{ENV['DEPLOY_USER']}@#{ENV['DEPLOY_SERVER']}"
