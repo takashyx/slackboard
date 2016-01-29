bac63ef548cb13c2d3514d88cef4e16328c025cf
takashyx <takashyx@gmail.com>
2016-01-29 16:36:38 +0900

    Fix mecab install script for circleCI

--- circle/install_mecab.sh
+++ circle/install_mecab.sh
@@ -1,39 +1,52 @@
-# Thanks to: http://qiita.com/saicologic/items/933e2f27b3e32c199248
+#!/bin/bash
 
-set -x
-set -e
-
-BASE_DIR="$PWD/vendor/mecab"
-
+# Remove mecab
 sudo apt-get remove mecab
 
-if [ ! -d "$BASE_DIR" ]
-then
-    mkdir $BASE_DIR
-fi
-
-if [ ! -f "$BASE_DIR/mecab-0.996.tar.gz" ]
-then
-    cd $BASE_DIR
-    wget http://mecab.googlecode.com/files/mecab-0.996.tar.gz
-    tar zxvf mecab-0.996.tar.gz
-    cd mecab-0.996
-    ./configure
-    make
-fi
-cd $BASE_DIR/mecab-0.996
-sudo make install
-sudo sh -c "echo '/usr/local/lib' >> /etc/ld.so.conf"
+# make sure
+sudo rm /usr/lib/libmecab.*
 sudo ldconfig
 
-if [ ! -f "$BASE_DIR/mecab-ipadic-2.7.0-20070801.tar.gz" ]
-then
-    cd $BASE_DIR
-    wget http://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
-    tar zxvf mecab-ipadic-2.7.0-20070801.tar.gz
-    cd mecab-ipadic-2.7.0-20070801
-    ./configure --with-charset=utf8
-    make
-fi
-cd $BASE_DIR/mecab-ipadic-2.7.0-20070801
+echo "========================================================================"
+
+
+mkdir -p ~/tmp
+
+# Install mecab
+
+cd ~/tmp
+curl -O https://mecab.googlecode.com/files/mecab-0.996.tar.gz
+tar zxfv mecab-0.996.tar.gz
+cd mecab-0.996
+./configure
+make
 sudo make install
+
+echo "========================================================================"
+
+# load mecab.so
+sudo sh -c "echo '$HOME/usr/lib' >> /etc/ld.so.conf"
+sudo ldconfig
+
+echo "========================================================================"
+
+# Install mecab-ipadic
+cd ~/tmp
+curl -O https://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
+tar zxfv mecab-ipadic-2.7.0-20070801.tar.gz
+cd mecab-ipadic-2.7.0-20070801
+./configure --with-charset=utf8
+make
+sudo make install
+
+# add to mecabrc
+sudo sh -c "echo 'dicrc = $HOME/usr/lib/mecab/dic/ipadic' >> /etc/mecabrc"
+
+echo "========================================================================"
+
+# Install mecab-python
+cd ~/tmp
+curl -O https://mecab.googlecode.com/files/mecab-python-0.996.tar.gz
+pip install --user mecab-python-0.996.tar.gz
+
+echo "========================================================================"
